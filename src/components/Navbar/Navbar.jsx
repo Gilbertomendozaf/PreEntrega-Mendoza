4fb7dc71133641d0d64f71575e64e25840854713
gilberto mendoza <gilbertomf2801@gmail.com>
2023-05-25 20:50:46 -0300

    2do commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import  "./Navbar.css";
+
+const Navbar = () => {
+  return (
+      <nav className='navbar'>
+        <a className='logo'><img src="" alt="" />Logo</a>
+        <ul className="nav-links">
+          <li className="nav-links">Productos</li>
+          <li className="nav-links"></li>
+          <li className="nav-links">contacto</li>
+        </ul>
+      </nav>
+  )
+}
+export default Navbar
